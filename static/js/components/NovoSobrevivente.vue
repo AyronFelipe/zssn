a77ba86d7392eb21b2566f6bf985e6fb103d4cf1
Ayron <ayron41@gmail.com>
2019-12-01 13:24:09 -0300

    Inputmask para os campos de latitude e longitude
    Signed-off-by: Ayron <ayron41@gmail.com>

--- static/js/components/NovoSobrevivente.vue
+++ static/js/components/NovoSobrevivente.vue
@@ -29,10 +29,54 @@
                                 <div class="card-header">
                                     <div class="card-title">Dados do novo sobrevivente</div>
                                 </div>
-                                <div class="card-body"></div>
-                                <div class="card-action">
-                                    <button class="btn btn-primary pull-right"><i class="fas fa-save mr-1"></i> Salvar</button>
-                                </div>
+                                <form>
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-lg-4 col-md-4 col-12">
+                                                <div class="form-group">
+                                                    <label for="nome">Nome <span class="required-label">*</span></label>
+                                                    <input required="" type="text" maxlength="255" placeholder="Digite seu nome" class="form-control" name="nome">
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4 col-md-4 col-12">
+                                                <div class="form-group">
+                                                    <label for="idade">Idade <span class="required-label">*</span></label>
+                                                    <input required="" type="number" min="0" placeholder="Digite sua idade" class="form-control" name="idade">
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4 col-md-4 col-12">
+                                                <div class="form-check">
+                                                    <label>Sexo <span class="required-label">*</span></label><br>
+                                                    <label class="form-radio-label">
+                                                        <input required="" class="form-radio-input" type="radio" name="sexo" value="M" checked="">
+                                                        <span class="form-radio-sign">Masculino</span>
+                                                    </label>
+                                                    <label class="form-radio-label ml-3">
+                                                        <input required="" class="form-radio-input" type="radio" name="sexo" value="F">
+                                                        <span class="form-radio-sign">Feminino</span>
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="latitude">Latitude <span class="required-label">*</span></label>
+                                                    <input required="" type="text" maxlength="10" placeholder="Digite a latitude da sua posição atual" class="form-control" name="latitude" id="latitude">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                                <div class="form-group">
+                                                    <label for="longitude">Longitude <span class="required-label">*</span></label>
+                                                    <input required="" type="text" maxlength="10" placeholder="Digite a longitude da sua posição atual" class="form-control" name="longitude" id="longitude">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-action">
+                                        <button type="submit" class="btn btn-primary pull-right"><i class="fas fa-save mr-1"></i> Salvar</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
@@ -45,12 +89,21 @@
 <script>
     import Header from './Header';
     import Sidebar from './Sidebar';
+    import Inputmask from "inputmask";
 
     export default {
         name: 'inicial',
         components: {
             Header,
             Sidebar
+        },
+        mounted: function() {
+            var selector = document.getElementById("latitude");
+            var im = new Inputmask("99° 99' 99\"");
+            im.mask(selector);
+            var selector = document.getElementById("longitude");
+            var im = new Inputmask("99° 99' 99\"");
+            im.mask(selector);
         }
     }
 </script>

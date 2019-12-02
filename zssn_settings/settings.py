57e2669112320f3c5ad214b40ed2cd5cf91fa642
Ayron <ayron41@gmail.com>
2019-12-02 03:43:15 -0300

    Signed-off-by: Ayron <ayron41@gmail.com>

--- zssn_settings/settings.py
+++ zssn_settings/settings.py
@@ -1,5 +1,7 @@
 import os
 from decouple import config, Csv
+import dj_database_url
+import django_heroku
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -56,16 +58,24 @@ REST_FRAMEWORK = {
 
 WSGI_APPLICATION = 'zssn_settings.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+if not DEBUG:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {

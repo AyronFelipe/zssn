337aa3cd89b7d01cc0c511729d1753f315ac98c2
Ayron <ayron41@gmail.com>
2019-12-02 09:30:57 -0300

    Signed-off-by: Ayron <ayron41@gmail.com>

--- zssn_settings/settings.py
+++ zssn_settings/settings.py
@@ -108,11 +108,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
+STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
 
 MIDDLEWARE_CLASSES = (
+
     'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
